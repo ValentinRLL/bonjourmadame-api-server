d96d1012df0dcac2b615e58756dcc7e57599b1c9
Jérémy CHABERNAUD <j.chabernaud@linkbynet.com>
2020-05-12 21:12:19 +0200

    Add travis action

---
+++ .travis/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+IMAGE_NAME="bonjourmadame-api-server"
+
+if [ ! -z "${DOCKER_USERNAME}" ]; then
+    # maintainer
+    echo "Build type: maintainer"
+    docker pull ${DOCKER_USERNAME}/${IMAGE_NAME}:latest
+    docker build --cache-from ${DOCKER_USERNAME}/${IMAGE_NAME}:latest -t ${DOCKER_USERNAME}/${IMAGE_NAME}:${TRAVIS_COMMIT} .
+    docker tag ${DOCKER_USERNAME}/${IMAGE_NAME}:${TRAVIS_COMMIT} ${DOCKER_USERNAME}/${IMAGE_NAME}:nightly
+else
+    # other
+    echo "Build type: other"
+    docker build -t local/${IMAGE_NAME}:${TRAVIS_COMMIT}
+fi
+
+docker container run -d --name ${IMAGE_NAME} -p 8080:8080 ${DOCKER_USERNAME:-local}/${IMAGE_NAME}:${TRAVIS_COMMIT}
+sleep 10
+docker ps | grep ${IMAGE_NAME} && echo "Container running successfully"
+curl -sL http://127.0.0.1:8080/ | egrep "Return a latest url picture for bonjourmadame" && echo "BonjourMadame API Server running successfully"
+docker logs ${IMAGE_NAME}
